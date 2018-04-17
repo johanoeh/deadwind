09017ec1c71fef6b2167af1e30f72baa492b4116
johan <johan.oh1980@gmail.com>
2018-04-17 19:06:54 +0200

    initilaizing repository

---
+++ deadwind/xparser.py
@@ -0,0 +1,34 @@
+import re
+from .events import Event
+
+class TimeParser:
+
+    def __init__(self, event):
+        self.event = event
+        self.timeRe = "[0-2]\d:[0-5]\d-[0-2]\d:[0-5]\d"
+        self.dateRe = "20\d\d-[0-1][1-9]-[0-3][0-9]"
+
+    def parse(self, timeStr, events):
+        timeStr = timeStr.replace('\n',' ')
+        timeStr = timeStr.strip()
+        tokens = timeStr.split(' ')
+        startMatch = re.match(self.dateRe, tokens[0])
+        if not startMatch:
+            return
+        startDate = startMatch.group();
+        for i in range(1,len(tokens)):
+            match = re.match(self.timeRe,tokens[i]);
+            if match:
+                timeTokens = match.group().split('-');
+                event = Event(
+                                 self.event.subject,
+                                 startDate,
+                                 timeTokens[0],
+                                 startDate,
+                                 timeTokens[1],
+                                 self.event.isAllDayEvent,
+                                 self.event.description,
+                                 self.event.location,
+                                 self.event.isPrivate
+                                 )
+                events.append(event);
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='deadwind',
+      version='0.2',
+      description='converts Medvinds xsls shedule data to csv',
+      url='',
+      author='Johan Oh',
+      author_email='johan.oh1980@gmail.com',
+      license='MIT',
+      packages=['deadwind'],
+      install_requires=['openpyxl'],
+      zip_safe=False)
