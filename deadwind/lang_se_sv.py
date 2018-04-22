654e687c6f6101d35a76de1dfb9917a75a0746d0
johan <johan.oh1980@gmail.com>
2018-04-22 15:06:47 +0200

    Moving text for labels, message, buttons etc.. to a separate script

---
+++ deadwind/lang_se_sv.py
@@ -0,0 +1,25 @@
+
+lang_settings = {
+          "settings" : "Inställningar",
+          "save" : "Spara",
+          "clear" : "Rensa",
+          "subject" : "Ämne",
+          "description" : "beskrivning",
+          "location" : "plats"
+         }
+
+lang_main = {
+    'browse':'Bläddra',
+    'file_path':'Filsökväg: ',
+    'convert':'konvertera',
+    'deadwind':'Motvind',
+    'settings':'Inställningar',
+    'save' : 'spara',
+    'quit':'Avsluta',
+    'file_not_found_h' : 'Filen kunde inte hittas',
+    'file_not_found_message' : 'Filen kunde inte hittas',
+    'no_file_selected_h' : 'Ingen fil vald',
+    'no_file_selected_m' : 'Vänligen välj en fil !',
+    'select_file_t' : 'Välj fil',
+    'conversion_success' : "konverteringen klar!"
+ }
--- deadwind/mainwindow.py
+++ deadwind/mainwindow.py
@@ -6,22 +6,14 @@ import logging
 import os
 from settings import SettingsController
 import settings
+import lang_se_sv
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-lang_main = {
-    'browse':'Bläddra',
-    'convert':'konvertera',
-    'deadwind':'Motvind',
-    'settings':'Inställningar',
-    'save' : 'spara',
-    'quit':'Avsluta'
- }
-
 class MainWindow:
     def __init__(self, lang):
-
         self.sc = ScheduleConverter()
+        self.lang = lang
         self.selectFileName=""
         self.addFileEntry = None
         self.saveFileEntry = None
@@ -33,7 +25,7 @@ class MainWindow:
         menubar.add_command(label=lang['settings'], command=self.openSettings)
         menubar.add_command(label=lang['quit'], command=root.quit)
         root.config(menu=menubar)
-        root.wm_title(lang_main['deadwind'])
+        root.wm_title(lang['deadwind'])
         root.iconbitmap(dir_path+r'\resources\cbt.ico')
 
         # Add a grid
@@ -44,17 +36,16 @@ class MainWindow:
         self.mainframe.pack(pady = 20, padx = 10)
 
         # first row of grid
-        self.labelSelect = tkinter.Label(self.mainframe, text="Filsökväg:")
+        self.labelSelect = tkinter.Label(self.mainframe, text=lang['file_path'])
         self.labelSelect.grid(row=0, column=0,padx="2")
         self.addFileEntry = tkinter.Entry(self.mainframe, bg='white', relief=SUNKEN,width=40)
         self.addFileEntry.grid(row=0,column=1, padx=2)
-        self.selectBTN = tkinter.Button(self.mainframe, text = lang_main['browse'], command = self.selectFile, width=20)
+        self.selectBTN = tkinter.Button(self.mainframe, text = lang['browse'], command = self.selectFile, width=20)
         self.selectBTN.grid(row=0, column=2, padx=2)
-        self.saveBTN = tkinter.Button(self.mainframe, text=lang_main['convert'],command = self.saveFile, width=20)
+        self.saveBTN = tkinter.Button(self.mainframe, text=lang['convert'],command = self.saveFile, width=20)
         self.saveBTN.grid(row=0, column=3 , padx=2)
         self.listBox = tkinter.Listbox(self.mainframe, bg='black',  fg='white')
         self.listBox.grid(row=1, columnspan=4, sticky=W+E+N+S)
-
         root.mainloop()
 
     def setText(self, text, entry):
@@ -67,29 +58,25 @@ class MainWindow:
 
     def selectFile(self):
         self.selectFileName = (
-        filedialog.askopenfilename(initialdir = dir_path,title = "Select file",filetypes = (("csv  files xlsx","*.xlsx"),("all files","*.*"))))
+        filedialog.askopenfilename(initialdir = dir_path,title = self.lang['select_file_t'],filetypes = (("csv  files xlsx","*.xlsx"),("all files","*.*"))))
         self.setText(self.selectFileName,self.addFileEntry)
 
     def saveFile(self):
         self.selectFileName = self.addFileEntry.get();
         if self.selectFileName == "" or self.selectFileName == None :
-            tkinter.messagebox.showinfo("Ingen fil vald"," Vänligen välj en  fil att konvertera!")
+            tkinter.messagebox.showinfo(self.lang['no_file_selected_h'],self.lang['no_file_selected_m'])
             return
-        self.saveFileName = filedialog.asksaveasfilename(initialdir = dir_path,title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+        self.saveFileName = filedialog.asksaveasfilename(initialdir = dir_path,title = self.lang['select_file_t'],filetypes = (("csv files","*.csv"),("all files","*.*")))
         if self.saveFileName == None or self.saveFileName =="":
             return
         self.listBox.delete(0,tkinter.END)
         try:
             events = ScheduleConverter().convert(self.selectFileName, self.saveFileName)
-            self.setText("konverteringen klar!", self.addFileEntry)
+            self.setText(self.lang['conversion_success'], self.addFileEntry)
             for event in events:
-                self.listBox.insert(END, event.toCSV())
+                self.listBox.insert(END, event.toList())
         except FileNotFoundError as e:
-            var = tkinter.messagebox.showinfo("Filen kunde inte hittas"," Fel filen med sökväg \' "+self.selectFileName+"  \' kunde inte hittas!")
+            var = tkinter.messagebox.showinfo(self.lang['file_not_found_h'],"\""+ self.selectFileName+" \" "+self.lang['file_not_found_message'])
         except Exception as e:
             logging.exception("Something went wrong while parsing the file")
-MainWindow(lang_main)
-
-class MainController():
-    def __init__(self, arg):
-        MainWindow(lang_main)
+MainWindow(lang_se_sv.lang_main)
